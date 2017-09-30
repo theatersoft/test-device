de578a5a995d46afd7ff3178bab3f6280c21364d
Rob Huehn <rhuehn@gmail.com>
2017-09-30 14:53:05 -0700

    add pageview webtask

--- build.js
+++ build.js
@@ -16,49 +16,51 @@ const
     babel = require('rollup-plugin-babel'),
     ignore = require('rollup-plugin-ignore'),
     nodeResolve = require('rollup-plugin-node-resolve'),
-    strip = require('rollup-plugin-strip')
+    strip = require('rollup-plugin-strip'),
+    commonjs = require('rollup-plugin-commonjs'),
+    includePaths = require('rollup-plugin-includepaths')
 
 const targets = {
     node () {
         console.log('target node')
         exec('mkdir -p dist')
         rollup({
-                entry: 'src/index.js',
-                external: [
-                    ...Object.keys(dependencies)
-                ],
-                plugins: [
-                    babel({
-                        babelrc: false,
-                        comments: !DIST,
-                        minified: DIST,
-                        //presets: [babili],
-                        plugins: [
-                            //require("babel-plugin-transform-class-properties"),
-                            [require("babel-plugin-transform-object-rest-spread"), {useBuiltIns: true}]
-                        ].concat(DIST ? [
-                            require("babel-plugin-minify-constant-folding"),
-                            //require("babel-plugin-minify-dead-code-elimination"), // FAIL NodePath has been removed so is read-only
-                            require("babel-plugin-minify-flip-comparisons"),
-                            require("babel-plugin-minify-guarded-expressions"),
-                            require("babel-plugin-minify-infinity"),
-                            require("babel-plugin-minify-mangle-names"),
-                            require("babel-plugin-minify-replace"),
-                            //FAIL require("babel-plugin-minify-simplify"),
-                            require("babel-plugin-minify-type-constructors"),
-                            require("babel-plugin-transform-member-expression-literals"),
-                            require("babel-plugin-transform-merge-sibling-variables"),
-                            require("babel-plugin-transform-minify-booleans"),
-                            require("babel-plugin-transform-property-literals"),
-                            require("babel-plugin-transform-simplify-comparison-operators"),
-                            require("babel-plugin-transform-undefined-to-void")
-                        ] : [])
-                    }),
-                    DIST && ignore(['remote-redux-devtools']),
-                    DIST && strip({functions: ['composeWithDevTools']}),
-                    nodeResolve({jsnext: true})
-                ]
-            })
+            entry: 'src/index.js',
+            external: [
+                ...Object.keys(dependencies)
+            ],
+            plugins: [
+                babel({
+                    babelrc: false,
+                    comments: !DIST,
+                    minified: DIST,
+                    //presets: [babili],
+                    plugins: [
+                        //require("babel-plugin-transform-class-properties"),
+                        [require("babel-plugin-transform-object-rest-spread"), {useBuiltIns: true}]
+                    ].concat(DIST ? [
+                        require("babel-plugin-minify-constant-folding"),
+                        //require("babel-plugin-minify-dead-code-elimination"), // FAIL NodePath has been removed so is read-only
+                        require("babel-plugin-minify-flip-comparisons"),
+                        require("babel-plugin-minify-guarded-expressions"),
+                        require("babel-plugin-minify-infinity"),
+                        require("babel-plugin-minify-mangle-names"),
+                        require("babel-plugin-minify-replace"),
+                        //FAIL require("babel-plugin-minify-simplify"),
+                        require("babel-plugin-minify-type-constructors"),
+                        require("babel-plugin-transform-member-expression-literals"),
+                        require("babel-plugin-transform-merge-sibling-variables"),
+                        require("babel-plugin-transform-minify-booleans"),
+                        require("babel-plugin-transform-property-literals"),
+                        require("babel-plugin-transform-simplify-comparison-operators"),
+                        require("babel-plugin-transform-undefined-to-void")
+                    ] : [])
+                }),
+                DIST && ignore(['remote-redux-devtools']),
+                DIST && strip({functions: ['composeWithDevTools']}),
+                nodeResolve({jsnext: true})
+            ]
+        })
             .then(bundle =>
                 bundle.write({
                         dest: `dist/${name}.js`,
@@ -68,6 +70,48 @@ const targets = {
                         sourceMap: DIST ? false : 'inline'
                     })
                     .then(() => console.log(`wrote dist/${name}.js`))
+            )
+    },
+
+    webtask () {
+        console.log('target node')
+        exec('mkdir -p dist')
+        rollup({
+            entry: 'webtask/index.js',
+            external: [
+                'util', 'ws'
+            ],
+            plugins: [
+                babel({
+                    babelrc: false,
+                    comments: !DIST,
+                    minified: DIST,
+                    plugins: [
+                        //require("babel-plugin-transform-class-properties"),
+                        [require("babel-plugin-transform-object-rest-spread"), {useBuiltIns: true}]
+                    ]
+                }),
+                //includePaths({
+                //    include: {
+                //        '@theatersoft/bus': '../node_modules/@theatersoft/bus/bus.js'
+                //    }
+                //}),
+                commonjs({
+                    include: [
+                        'node_modules/**'
+                    ]
+                })
+            ]
+        })
+            .then(bundle =>
+                bundle.write({
+                        dest: `webtask/dist/pageview.js`,
+                        format: 'cjs',
+                        moduleName: 'webtask',
+                        banner: copyright,
+                        sourceMap: false
+                    })
+                    .then(() => console.log(`wrote dist/pageview.js`))
             )
     },
 
