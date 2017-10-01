61f84c8af4e05a766681ad7a0128f80d872f68f2
Rob Huehn <rhuehn@gmail.com>
2017-09-30 18:33:15 -0700

    use renamed .bus secrets, use query params

--- webtask/index.js
+++ webtask/index.js
@@ -1,12 +1,11 @@
-import {bus} from '../node_modules/@theatersoft/bus/bus.js'
+import {bus, proxy, log} from '../node_modules/@theatersoft/bus/bus.js'
 
 export default function (context, callback) {
-    const {url, auth} = context.secrets
+    const
+        {BUS: url, AUTH: auth} = context.secrets,
+        {name = 'Home', id = '/'} = context.data
     bus.start({parent: {url, auth}})
-        .then(() => {
-            log('PageView.pulse')
-            return bus.proxy('PageView').pulse({name: 'Home', id: '/'})
-        })
+        .then(() => proxy('PageView').pulse({name, id}))
         .catch(e => log(e))
         .then(bus.close)
 }
